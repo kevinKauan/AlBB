59307313798268b14d095095f62be326d2e69764
KevinKauan1998 <kevinkauan1998@gmail.com>
2025-06-06 19:27:35 -0300

    git commit

---
+++ script.js
@@ -0,0 +1,8 @@
+  function asideItem() {
+    const altera = document.getElementById("asd-pai");
+    if (altera.style.display === "none") {
+      altera.style.display = "block";
+    } else {
+      altera.style.display = "none";
+    }
+  }
