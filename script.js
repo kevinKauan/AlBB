b20d6879c0fdac59552593c23de817f17b442894
KevinKauan1998 <kevinkauan1998@gmail.com>
2025-06-07 23:07:44 -0300

    git commit

--- script.js
+++ script.js
@@ -6,3 +6,12 @@
       altera.style.display = "none";
     }
   }
+
+
+      import { animate, hover } from "https://cdn.jsdelivr.net/npm/motion@12.16.0/+esm"
+
+    hover(".card-son", (element) => {
+        animate(element, { scale: 1.1 })
+
+        return () => animate(element, { scale: 1 })
+    })
