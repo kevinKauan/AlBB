b4ec50c0951af5474d99ad681782c581ffeb5938
KevinKauan1998 <kevinkauan1998@gmail.com>
2025-06-14 23:27:14 -0300

    git commit

--- script.js
+++ script.js
@@ -6,3 +6,11 @@
       altera.style.display = "none";
     }
   }
+
+function calculo() {
+  const mani = document.getElementById("ID DO INPUT");
+  const valor = parseFloat(mani.value); // Converte o valor para número
+  const resultado = valor * 3;
+  mani.value = resultado; // Atualiza o input com o novo valor
+  return resultado;
+}
